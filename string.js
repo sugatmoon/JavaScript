6fcd259f2cb6c6859f117cb3b45e0ed73a61ae7c
sugat <sugatmoon17@gmail.com>
2023-03-19 07:09:36 +0530

    A string file adding with their oparation

---
+++ string.js
@@ -0,0 +1,29 @@
+var s = "This is javaScript file writtern by @sugatmoon" ;
+
+var x = s.substring( 10 );
+  
+
+console.log( x );
+
+
+var fruits = 'Apple,Banana,Graphs';
+
+var newFruits = fruits.split( ',' );
+ 
+var newfruits = fruits.replace( 'Graphs', 'Papai' );
+
+console.log( newFruits );
+console.log( fruits );
+console.log( newfruits );
+
+
+
+var indexof = x.indexOf( "file" );
+var lastindexof = x.lastIndexOf( 'file' );
+var anotherindexof = x.indexOf( 'n' );
+
+console.log( indexof );
+console.log( lastindexof );
+console.log( anotherindexof );
+
+
