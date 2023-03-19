ceb281ea80ec6f62600682990b2cfb9df4ca3369
sugat <sugatmoon17@gmail.com>
2023-03-19 07:08:47 +0530

    math functioins file

---
+++ math.js
@@ -0,0 +1,16 @@
+var num = Math.random();
+
+console.log( num );
+
+
+console.log( Math.min( 2,12,3,5,46,8,49,9,97,9) );
+console.log( Math.max( 2,12,3,5,46,8,49,9,97,9) );
+
+
+var numbers = [ 2,12,3,5,46,8,49,9,97,9 ];
+var numbers1 = [ 3,1,6,59,79,2,1,67,566666,9];
+ 
+
+
+
+console.log( Math.max.apply( numbers, numbers1) );  
