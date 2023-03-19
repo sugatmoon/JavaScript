c2c7d1cbf18247502043f66e661d611d9703fb70
sugat <sugatmoon17@gmail.com>
2023-03-19 07:11:39 +0530

    MOdifiled array method

--- arrayMethod.js
+++ arrayMethod.js
@@ -5,3 +5,31 @@ var days = [
     'Friday',
     'Satersday'
 ];
+days.sort();
+
+days.reverse();
+ 
+days[days.length] = 'Wenesday';
+days[days.length] = 'sunday';
+
+console.log(days.length );
+
+console.log( 1 + days.indexOf('sunday') );
+console.log( days );
+ 
+
+
+
+days.push( 'NODAY' );
+days.sort();
+console.log( days );
+
+
+
+days.pop();
+console.log( days );
+ 
+
+var x = days.pop();
+ 
+console.log( x );
